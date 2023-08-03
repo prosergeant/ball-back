2c8ac27d451e4f52e6551d1edb6d9a963d4e32fd
prosergeant <sberendeyev@gmail.com>
2023-08-03 20:19:01 +0600

    close pg ports

--- ballback/settings.py
+++ ballback/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'zYv280IeT45^',
+        'PASSWORD': 'postgres',
         'HOST': 'pgdb',
         'PORT': '5432',
     }
