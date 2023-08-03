a8a2190e523b9373387427b90fdcbd63e6fa7d75
prosergeant <sberendeyev@gmail.com>
2023-08-03 20:13:00 +0600

    change pg password

--- ballback/settings.py
+++ ballback/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'zYv280IeT45^',
         'HOST': 'pgdb',
         'PORT': '5432',
     }
