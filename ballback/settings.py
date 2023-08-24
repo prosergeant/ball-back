573b9c285738a5995b9bf97b71d509281010f679
prosergeant <sberendeyev@gmail.com>
2023-08-24 12:42:25 +0600

    test media_url

--- ballback/settings.py
+++ ballback/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://bronkz.app/api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
