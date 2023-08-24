55385af97f733659cdda9257ef1e3c36e6c6634e
prosergeant <sberendeyev@gmail.com>
2023-08-24 13:52:45 +0600

    -static root

--- ballback/settings.py
+++ ballback/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'https://bronkz.app/api/static/'
-MEDIA_URL = 'https://bronkz.app/api/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL  = 'https://bronkz.app/api/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL  = 'static/'
+
+MEDIA_URL   = 'https://bronkz.app/api/media/'
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
