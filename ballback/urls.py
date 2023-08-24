e19df4886283d8366b8ecd9a26e1276e4cf1f727
prosergeant <sberendeyev@gmail.com>
2023-08-24 13:22:23 +0600

    static root

--- ballback/urls.py
+++ ballback/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .settings import MEDIA_ROOT, MEDIA_URL
+from .settings import MEDIA_ROOT, MEDIA_URL, STATIC_ROOT, STATIC_URL
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
+  + static(STATIC_URL, document_root=STATIC_ROOT)
