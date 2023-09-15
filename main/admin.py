69bc15cc4e9104f2dee1b1eab5c5bd58d1a8d7b5
prosergeant <sberendeyev@gmail.com>
2023-09-15 17:51:08 +0600

    FieldPhoto in admin

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,7 @@ from .models import *
 
 admin.site.register(DefUser)
 admin.site.register(Field)
+admin.site.register(FieldPhoto)
 admin.site.register(FieldType)
 admin.site.register(Request)
 admin.site.register(Tag)
